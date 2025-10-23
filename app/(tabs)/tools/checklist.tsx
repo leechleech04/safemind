8a7c50a11d4be01a9bdc24d93a3410128b108702
lch2023 <lch2023@naver.com>
2025-10-23 23:13:17 +0900

    Design 비상용품 스크린

--- app/(tabs)/tools/checklist.tsx
+++ app/(tabs)/tools/checklist.tsx
@@ -1,5 +1,161 @@
+import Header from '@/components/Header';
+import colors from '@/utils/colors';
+import { BasicContainer } from '@/utils/utilComponents';
+import { Ionicons } from '@expo/vector-icons';
+import AsyncStroage from '@react-native-async-storage/async-storage';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components/native';
+
 const Checklist = () => {
-  return null;
+  interface ChecklistItemType {
+    sectors: { name: String; items: { label: String; isChecked: Boolean }[] }[];
+  }
+
+  const [checklistItems, setChecklistItems] =
+    useState<ChecklistItemType | null>(null);
+
+  const [progressPercentage, setProgressPercentage] = useState(0);
+
+  useEffect(() => {
+    const getChecklistItems = async () => {
+      try {
+        const response = await AsyncStroage.getItem('@safemind/checklist');
+        if (response) {
+          const parsedResponse = JSON.parse(response);
+          setChecklistItems(parsedResponse);
+        } else {
+          console.log('No checklist items found in storage.');
+        }
+      } catch (error) {
+        console.error('Error retrieving checklist items:', error);
+      }
+    };
+
+    getChecklistItems();
+  }, []);
+
+  return (
+    <BasicContainer>
+      <Header title="비상용품 체크리스트" />
+      <ProgressTextContainer>
+        <ProgressText>진행률</ProgressText>
+        <ProgressPercentageText>{progressPercentage}%</ProgressPercentageText>
+      </ProgressTextContainer>
+      <ProgressBarContainer>
+        <ProgressBarFill progress={progressPercentage} />
+      </ProgressBarContainer>
+      <ScrollContainer>
+        {checklistItems?.sectors.map((sector, sectorIndex) => (
+          <SectorContainer key={sectorIndex}>
+            <SectorTitle>{sector.name}</SectorTitle>
+            {sector.items.map((item, itemIndex) => (
+              <ItemContainer key={itemIndex}>
+                <ItemCheckButton>
+                  <Ionicons
+                    name={item.isChecked ? 'checkbox' : 'square-outline'}
+                    size={24}
+                    color={item.isChecked ? colors.blue : colors.white}
+                  />
+                </ItemCheckButton>
+                <ItemLabel isChecked={item.isChecked}>{item.label}</ItemLabel>
+                <ItemDeleteButton>
+                  <Ionicons name="trash-outline" size={24} color={colors.red} />
+                </ItemDeleteButton>
+              </ItemContainer>
+            ))}
+          </SectorContainer>
+        ))}
+        <AddItemContainer>
+          <Ionicons name="add-circle-outline" size={24} color={colors.blue} />
+          <AddItemText>직접 항목 추가하기</AddItemText>
+        </AddItemContainer>
+      </ScrollContainer>
+    </BasicContainer>
+  );
 };
+
+const ProgressTextContainer = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-top: 24px;
+`;
+
+const ProgressText = styled.Text`
+  color: ${colors.white};
+  font-size: 16px;
+`;
+
+const ProgressPercentageText = styled.Text`
+  color: ${colors.white};
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+const ProgressBarContainer = styled.View`
+  height: 10px;
+  width: 100%;
+  background-color: ${colors.blue + '22'};
+  border-radius: 5px;
+  margin-top: 16px;
+  margin-bottom: 12px;
+`;
+
+const ProgressBarFill = styled.View<{ progress: number }>`
+  height: 100%;
+  width: ${({ progress }: { progress: number }) => progress}%;
+  background-color: ${colors.blue};
+  border-radius: 5px;
+`;
+
+const ScrollContainer = styled.ScrollView`
+  flex: 1;
+  width: 100%;
+`;
+
+const SectorContainer = styled.View`
+  margin-top: 32px;
+`;
+
+const SectorTitle = styled.Text`
+  color: ${colors.white};
+  font-size: 22px;
+  font-weight: bold;
+`;
+
+const ItemContainer = styled.View`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 24px;
+`;
+
+const ItemCheckButton = styled.Pressable``;
+
+const ItemLabel = styled.Text<{ isChecked: boolean }>`
+  color: ${({ isChecked }: { isChecked: boolean }) =>
+    isChecked ? colors.lightGray : colors.white};
+  font-size: 16px;
+  margin-left: 12px;
+`;
+
+const ItemDeleteButton = styled.Pressable`
+  margin-left: auto;
+`;
+
+const AddItemContainer = styled.Pressable`
+  flex-direction: row;
+  align-items: center;
+  margin-top: 24px;
+  border-top-width: 0.5px;
+  border-top-color: ${colors.blue};
+  padding-top: 12px;
+`;
+
+const AddItemText = styled.Text`
+  color: ${colors.lightGray};
+  font-size: 16px;
+  margin-left: 12px;
+`;
 
 export default Checklist;
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,171 @@
 import ReduxProvider from '@/ReduxProvider';
 import colors from '@/utils/colors';
+import AsyncStroage from '@react-native-async-storage/async-storage';
 import { Slot } from 'expo-router';
+import { useEffect } from 'react';
 import styled from 'styled-components/native';
 
-export default function RootLayout() {
+const RootLayout = () => {
+  useEffect(() => {
+    const setChecklistItems = async () => {
+      try {
+        const response = await AsyncStroage.getItem('@safemind/checklist');
+        if (!response) {
+          const defaultChecklist = {
+            sectors: [
+              {
+                name: '생존 필수품',
+                items: [
+                  {
+                    label: '식수 6L',
+                    isChecked: false,
+                  },
+                  {
+                    label: '비상식량 6000–8000kcal',
+                    isChecked: false,
+                  },
+                  {
+                    label: '보조배터리 10,000mAh+ 2개',
+                    isChecked: false,
+                  },
+                  {
+                    label: '손전등/헤드램프 + 예비건전지',
+                    isChecked: false,
+                  },
+                  {
+                    label: '휴대 라디오(크랭크 겸용)',
+                    isChecked: false,
+                  },
+                  {
+                    label: '멀티툴',
+                    isChecked: false,
+                  },
+                  {
+                    label: '현금 소액',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '응급·의약품',
+                items: [
+                  {
+                    label: '개인 복용약 7일분',
+                    isChecked: false,
+                  },
+                  {
+                    label: '소독제·거즈·붕대·밴드·테이프',
+                    isChecked: false,
+                  },
+                  {
+                    label: '진통/해열·지사제·항히스타민',
+                    isChecked: false,
+                  },
+                  {
+                    label: '체온계',
+                    isChecked: false,
+                  },
+                  {
+                    label: '일회용 장갑',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '위생·보호',
+                items: [
+                  {
+                    label: 'KF94 마스크',
+                    isChecked: false,
+                  },
+                  {
+                    label: '손 소독제·물티슈',
+                    isChecked: false,
+                  },
+                  {
+                    label: '위생백·휴지',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '도구·정비/취사',
+                items: [
+                  {
+                    label: '가스버너 + 부탄 2캔',
+                    isChecked: false,
+                  },
+                  {
+                    label: '라이터 2개/방수 성냥',
+                    isChecked: false,
+                  },
+                  {
+                    label: '접이 컵·수저·물통(2–5L)',
+                    isChecked: false,
+                  },
+                  {
+                    label: '미니 공구·덕트테이프·케이블타이',
+                    isChecked: false,
+                  },
+                  {
+                    label: '호루라기·방수포·로프',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '의복·보온',
+                items: [
+                  {
+                    label: '보온포/담요',
+                    isChecked: false,
+                  },
+                  {
+                    label: '여벌 속옷/양말(압축)',
+                    isChecked: false,
+                  },
+                  {
+                    label: '장갑(작업/보온), 모자, 튼튼한 신발',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '통신·정보·문서',
+                items: [
+                  {
+                    label: '비상 연락망 카드',
+                    isChecked: false,
+                  },
+                  {
+                    label: '신분증·등본 사본(방수팩)',
+                    isChecked: false,
+                  },
+                  {
+                    label: '오프라인 지도·나침반',
+                    isChecked: false,
+                  },
+                ],
+              },
+              {
+                name: '기타',
+                items: [],
+              },
+            ],
+          };
+          await AsyncStroage.setItem(
+            '@safemind/checklist',
+            JSON.stringify(defaultChecklist)
+          );
+        }
+      } catch (error) {
+        console.error('Error setting default checklist items:', error);
+      }
+    };
+
+    setChecklistItems();
+  }, []);
+
   return (
     <ReduxProvider>
       <Container>
@@ -11,9 +173,11 @@ export default function RootLayout() {
       </Container>
     </ReduxProvider>
   );
-}
+};
 
 const Container = styled.View`
   flex: 1;
   background-color: ${colors.backgroundColor};
 `;
+
+export default RootLayout;
